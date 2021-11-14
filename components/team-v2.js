1ab6d528750ac6487d44e8fad4895977d4e48e2d
Andrew Kessinger <andrew.kessinger@gmail.com>
2021-11-14 04:32:28 +0100

    last test

--- components/team-v2.js
+++ components/team-v2.js
@@ -8,15 +8,15 @@ const people = [
       name: 'Juan Pablo Domenech',
       role: 'Director',
       imageUrl:
-        'https://res.cloudinary.com/annaga/image/upload/v1636733067/image-w856_pdmbk9.jpg',
+        'https://res.cloudinary.com/annaga/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1636733067/image-w856_pdmbk9.jpg',
       twitterUrl: '#',
       linkedinUrl: '#',
     },
     {
-        name: 'Juan Pablo Domenech',
-        role: 'Director',
+        name: 'Noboru de Abreu',
+        role: 'Producer',
         imageUrl:
-          'https://res.cloudinary.com/annaga/image/upload/v1636733067/image-w856_pdmbk9.jpg',
+          'https://res.cloudinary.com/annaga/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1636860079/IMG_2514_dbyaku.jpg',
         twitterUrl: '#',
         linkedinUrl: '#',
       },
@@ -47,7 +47,12 @@ const people = [
     // More people...
   ]
 
-export default function Contact() {
+
+  
+
+
+
+export default function Team() {
 
   return (
     <>
@@ -62,7 +67,7 @@ export default function Contact() {
 
           <div className="grid grid-cols-1 lg:grid-cols-3">
             {/* Contact information */}
-            <div className="relative overflow-hidden bg-main-primary py-10 px-6 sm:px-12 xl:p-14">
+            <div className="relative overflow-hidden bg-main-primary py-10 px-6 sm:px-12 xl:p-14 pb-14 sm:pb-16">
               <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
                 <svg
                   className="absolute inset-0 w-full h-full"
@@ -159,78 +164,81 @@ export default function Contact() {
                   </defs>
                 </svg>
               </div>
-
-              <h3 className="text-3xl lg:text-4xl pt-14 sm:pt-16 md:pt-16 font-medium text-white"
+              <h3 className="text-3xl lg:text-4xl pt-20 font-medium text-white"
               
               style={{
                 fontFamily: "'Roboto', sans-serif" 
                          }}
               
               >About us</h3>
-              <p className="mt-6 text-2xl text-base text-gray-200 max-w-3xl">
+              <p className="mt-6 text-base text-gray-200 max-w-3xl">
                 Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor
-                lacus arcu.
+                lacus arcu. Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.
               </p>
-              </div>
+              
+             </div>
 
             {/* Contact form */}
-            <div className="lg:col-span-2 xl:p-12">
-              
-            <>
-     
-      <div className="bg-gray-50">
-        <div className="mx-auto pt-18 py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24   ">
-          <div className="space-y-12">
+            <div className="py-10 px-6 sm:px-12 xl:p-14 lg:col-span-2 bg-gray-50">
+              <h3 className="text-2xl lg:text-2xl pt-3 sm:pt-5 lg:pt-20 font-medium text-main-primary"
+                style={{
+                    fontFamily: "'Roboto', sans-serif" 
+                             }}
+              >Our team</h3>
+             
+             <div className="bg-transparent">
+      <div className="mx-auto pt-8 max-w-7xl">
+        <div className="space-y-12">
             <ul
-              role="list"
-              className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
-            >
-              {people.map((person) => (
-                <li key={person.name}>
-                  <div className="space-y-4">
-                    <div className="aspect-w-3 aspect-h-2">
-                      <img className="object-cover shadow-lg rounded-lg" src={person.imageUrl} alt="" />
-                    </div>
-  
-                    <div className="space-y-2">
-                      <div className="text-lg leading-6 font-medium space-y-1">
-                        <h3>{person.name}</h3>
-                        <p className="text-main-primary">{person.role}</p>
-                      </div>
-                      <ul role="list" className="flex space-x-5">
-                        <li>
-                          <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
-                            <span className="sr-only">Twitter</span>
-                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                          </a>
-                        </li>
-                        <li>
-                          <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
-                            <span className="sr-only">LinkedIn</span>
-                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path
-                                fillRule="evenodd"
-                                d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
+            role="list"
+            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="space-y-4">
+                  <div className="aspect-w-3 aspect-h-2">
+                    <img className="object-cover shadow-lg rounded-lg" src={person.imageUrl} alt="" />
                   </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+
+                  <div className="space-y-2">
+                    <div className="text-lg leading-6 font-medium space-y-1">
+                      <h3>{person.name}</h3>
+                      <p className="text-main-primary">{person.role}</p>
+                    </div>
+                    <ul role="list" className="flex space-x-5">
+                      <li>
+                        <a href={person.twitterUrl} className="text-gray-400 hover:text-main-primary">
+                          <span className="sr-only">Twitter</span>
+                          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href={person.linkedinUrl} className="text-gray-400 hover:text-main-primary">
+                          <span className="sr-only">LinkedIn</span>
+                          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                              fillRule="evenodd"
+                              d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      </>
-
-              
+    </div>
+             
+             
             </div>
+            
           </div>
         </div>
       </div>
@@ -239,3 +247,4 @@ export default function Contact() {
 </>
   )
 }
+
