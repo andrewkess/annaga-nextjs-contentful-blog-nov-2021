5f844eed01efc9f1a51ea5b10475e03e672602ad
Andrew Kessinger <andrew.kessinger@gmail.com>
2021-11-15 02:02:11 +0100

    full

--- components/post-preview.js
+++ components/post-preview.js
@@ -14,7 +14,7 @@ export default function PostPreview({
   return (
     <>
     <div>
-      <div className="mx-12 md:mx-0 bg-red-900">
+      <div className="md:mx-0 bg-red-900">
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
