d0344e913c7c47e7bdb38f56caf00fb74a841035
Andrew Kessinger <andrew.kessinger@gmail.com>
2021-12-31 18:24:25 -0300

    font

--- components/post-preview.js
+++ components/post-preview.js
@@ -36,25 +36,19 @@ var modifiedTitle = titleArr.join(' ');
     <CoverImage title={title} slug={slug} url={coverImage.url} />
 
     <div className="z-10 py-6 text-white/90 hover:text-white/100 absolute inset-x-0 bottom-0  hover:bg-black hover:bg-opacity-20">
-      <div className="absolute flex justify-between items-end inset-x-0 bottom-0 leading-none bg-gradient-to-b from-transparent to-black
+      <div className="absolute flex flex-wrap sm:flex-nowrap justify-between items-end inset-x-0 bottom-0 leading-none bg-gradient-to-b from-transparent to-black
       text-4xl sm:text-6xl 2xl:text-7xl p-2 sm:p-4 lg:p-6 drop-shadow-lg tracking-tight md:tracking-normal
       "
       style={{
         fontFamily: "'Rubik', sans-serif",
-      }}
-      
-      >
+      }} >
         
-        <div className="">{
-
-modifiedTitle
-
-            }</div> 
+    <div className="">{modifiedTitle}</div> 
       
-
-      <div className="text-xl sm:text-2xl lg:text-3xl bg-gray-900 bg-opacity-10 rounded"   style={{
+      <div className="text-xl sm:text-2xl lg:text-3xl bg-gray-900 bg-opacity-10 rounded text-right"   style={{
         fontFamily: "'Open Sans Condensed', sans-serif",
       }}><DateComponent dateString={date} /></div>
+
 </div>
     </div>
     
