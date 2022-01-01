a950a32b81d5342f348e21f05ef3527a9648a5a6
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 11:53:29 -0300

    next gen formats

--- components/CarouselData.js
+++ components/CarouselData.js
@@ -1,28 +1,21 @@
 export const CarouselData = [
     {
       image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_auto/v1636486533/ramy-keyframe_zdjnqu.jpg",
+        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/v1636486533/ramy-keyframe_zdjnqu.jpg",
         title: "Law of desire",
         blurb: "Fun that's as explosively bright and provocative as ever."
     },
     {
       image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_auto/v1636486535/king-keyframe_dretwk.jpg",
-        title: "Penguin bloom",
-        blurb: "Graceful comment on timely sociopolitical themes."
-
-    },
-    {
-      image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_auto/v1636486532/spy_emrbfg.jpg",
+        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/v1636486532/spy_emrbfg.jpg",
         title: "Desert Storm story",
         blurb: "A magical journey about the power of imagination."
     },
     {
       image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_auto/v1636486537/bear_gsipdp.jpg",
+        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/v1636486537/bear_gsipdp.jpg",
         title: "Bears and cubs",
-        blurb: "Where have you seen a more gorgeous bear in the world?"
+        blurb: "Graceful comment on timely sociopolitical themes."
 
     },
   ];
