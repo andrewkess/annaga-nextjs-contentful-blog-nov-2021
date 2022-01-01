111dd09f87cedf2d608a54c7872c7c17364776d2
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 12:49:39 -0300

    update

--- components/CarouselData.js
+++ components/CarouselData.js
@@ -1,13 +1,13 @@
 export const CarouselData = [
     {
       image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/v1636486533/ramy-keyframe_zdjnqu.jpg",
+        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/c_fill,h_1600,w_2400/v1636486533/ramy-keyframe_zdjnqu.jpg",
         title: "Law of desire",
         blurb: "Fun that's as explosively bright and provocative as ever."
     },
     {
       image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/v1636486532/spy_emrbfg.jpg",
+        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/c_fill,h_1600,w_2400/v1636486532/spy_emrbfg.jpg",
         title: "Desert Storm story",
         blurb: "A magical journey about the power of imagination."
     },
