86c90e586f8266176ff958a0dbdad562bd3a6f33
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 21:58:58 -0300

    update

--- components/Carousel.js
+++ components/Carousel.js
@@ -17,15 +17,15 @@ class Carousel extends Component {
 
   componentDidMount() {
 
-    // setInterval(() => {
-    //   if (this.state.paused === false) {
-    //     let newSlide =
-    //       this.state.currentSlide === CarouselData.length - 1
-    //         ? 0
-    //         : this.state.currentSlide + 1;
-    //     this.setState({ currentSlide: newSlide });
-    //   }
-    // }, 4000);
+    setInterval(() => {
+      if (this.state.paused === false) {
+        let newSlide =
+          this.state.currentSlide === CarouselData.length - 1
+            ? 0
+            : this.state.currentSlide + 1;
+        this.setState({ currentSlide: newSlide });
+      }
+    }, 4000);
   }
 
   nextSlide = () => {
