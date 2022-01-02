0023d9630e6aa6044f18b6ea2ac63753ba1495b1
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-02 01:29:57 -0300

    done

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,6 @@ export default function Index({ preview, allPosts }) {
         <Head>
           <title>Annaga productions</title>
         </Head>
-
         <Carousel />
         <Navbar />
         <Container>
