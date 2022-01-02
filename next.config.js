7f7b8d100aaf517acd935c55ec91513ac94c8046
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 22:17:34 -0300

    CRS

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 // You can choose which headers to add to the list
 // after learning more below.
-// const securityHeaders = [
-// {
-//   key: 'Content-Security-Policy',
-//   value: "default-src 'self' vitals.vercel-insights.com; img-src 'self' data: res.cloudinary.com images.ctfassets.net; font-src fonts.gstatic.com; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' 'unsafe-eval'"
-// }]
-
 const securityHeaders = [
-  {
+{
   key: 'Content-Security-Policy',
-  value: ""
-}
-];
+  value: "default-src 'self' vitals.vercel-insights.com; img-src 'self' data: res.cloudinary.com images.ctfassets.net; font-src fonts.gstatic.com; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' 'unsafe-eval'"
+}]
+
+// const securityHeaders = [
+//   {
+//   key: 'Content-Security-Policy',
+//   value: ""
+// }
+// ];
 
 
 module.exports = {
