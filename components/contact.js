75e69d7015a389ba0cc6d7c327841c3dbacdde66
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 23:00:07 -0300

    updated

--- components/contact.js
+++ components/contact.js
@@ -298,7 +298,7 @@ export default function Contact() {
                 <div className="sm:col-span-2 sm:flex sm:justify-end">
                   <button
                     type="submit"
-                    className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-primary/50 hover:bg-main-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-primary sm:w-auto"
+                    className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-primary/70 hover:bg-main-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-primary sm:w-auto"
                   >
                     Send
                   </button>
--- pages/contact.js
+++ pages/contact.js
@@ -4,21 +4,6 @@ import Layout from '../components/layout'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
-//hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
-if (typeof window !== "undefined") {
-  // browser code
-
-  function appHeight() {
-      const doc = document.documentElement
-      doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-    }
-  
-    window.addEventListener('resize', appHeight);
-    appHeight();
-
-}
-
-
 export default function Contact() {
 
     return (
