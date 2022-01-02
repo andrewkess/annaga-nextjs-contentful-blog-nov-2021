33e53e7425d8fc20b646f333af1a9b9e333ed863
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-01 22:28:28 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 // import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
+//import Intro from '../components/intro'
 import Navbar from '../components/navbar'
 import Carousel from '../components/Carousel'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+//import { CMS_NAME } from '../lib/constants'
 
 
 
@@ -16,23 +16,7 @@ export default function Index({ preview, allPosts }) {
 //  const heroPost = allPosts[0]
   //const morePosts = allPosts.slice(1)
   const morePosts = allPosts
-
-
-//hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
-if (typeof window !== "undefined") {
-  // browser code
-
-  function appHeight() {
-      const doc = document.documentElement
-      doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-    }
   
-    window.addEventListener('resize', appHeight);
-    appHeight();
-
-}
-
-
   return (
     <>
       <Layout preview={preview}>
