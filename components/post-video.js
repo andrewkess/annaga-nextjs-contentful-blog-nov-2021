a87d4b4b07bff1a375908ba3e3902fb55b8c91b1
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-06 21:07:36 -0300

    clear it out

--- components/post-video.js
+++ components/post-video.js
@@ -8,21 +8,9 @@ export default function PostVideo({ title, coverImage }) {
     //console.log(video);
     return (
     <>
-      <div className="m-0">
-
-<div
-  className="w-screen h-screen h-max-full flex overflow-hidden relative bg-red-400"
-  style={{
-    height:
-      '100vh' /* Fallback for browsers that do not support Custom Properties */,
-    height: 'calc(var(--vh, 1vh) * 100)',
-  }}
->
-    
+      {/* <div className="m-0">
 {coverImage && <ProjectImage url={coverImage.url} title={title} />}
-
-</div>
-</div>
+</div> */}
 {/* <div>Video: { video && video[0].url }</div> */}
 <VideoPlayer />
 
