99ac6b2e63c5657037ef830d44a39f8e296dfa24
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-15 19:26:14 +0100

    navbar image height

--- components/navbar.js
+++ components/navbar.js
@@ -48,7 +48,7 @@ export default function Navbar() {
               <div className="flex-1 flex items-center justify-start sm:items-stretch sm:justify-between">
                 <div className="flex-shrink-0 flex items-center z-50">
                   <a href="/" key="testwindow"><img
-                    className="block h-[40px] w-auto"
+                    className="block h-[30px] w-[160px] sm:h-[40px] sm:w-[200px]"
                     src="https://res.cloudinary.com/annaga/image/upload/v1636486534/annaga-logo_du1dlj.png"
                     alt="Annaga"
                   /></a>
