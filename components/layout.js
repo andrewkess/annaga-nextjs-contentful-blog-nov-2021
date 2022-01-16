e66dfcca05e742f503f378f4adb33e39a1888d5e
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-16 18:45:21 +0100

    added hack to layout

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,21 @@
 import Footer from '../components/footer'
 import Meta from '../components/meta'
 
+
+//hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
+if (typeof window !== "undefined") {
+  // browser code
+
+  function appHeight() {
+      const doc = document.documentElement
+      doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+    }
+  
+    window.addEventListener('resize', appHeight);
+    appHeight();
+
+}
+
 export default function Layout({ preview, children }) {
   return (
     <>
