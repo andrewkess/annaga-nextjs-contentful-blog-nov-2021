379ee92740b358ac3cccf0a11803c931626a0fb5
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-16 18:40:55 +0100

    feel

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,6 @@ import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 
 
-
-//hack to calculate the actual browser window height on mobile and save that value in the 'vh' variable which can be used later in CSS
-if (typeof window !== "undefined") {
-  // browser code
-
-  function appHeight() {
-      const doc = document.documentElement
-      doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-    }
-  
-    window.addEventListener('resize', appHeight);
-    appHeight();
-
-}
-
-
 export default function Index({ preview, allPosts }) {
 //  const heroPost = allPosts[0]
   //const morePosts = allPosts.slice(1)
