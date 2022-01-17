307fa3f38ebec895b82ccb97b2556a4a8104e6af
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-17 22:05:25 +0100

    amp

--- components/meta.js
+++ components/meta.js
@@ -44,10 +44,10 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=" " />
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&amp;display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&amp;display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Rubik&amp;display=swap" rel="stylesheet" />
 
     </Head>
   )
