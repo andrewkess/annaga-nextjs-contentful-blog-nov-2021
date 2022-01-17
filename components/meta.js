3e84e8f2177891a2da8b7869d9384a2f8e381549
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-17 21:39:54 +0100

    meta

--- components/meta.js
+++ components/meta.js
@@ -33,7 +33,7 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-
+{/* 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
@@ -44,7 +44,7 @@ export default function Meta() {
         name="description"
         content={`A statically generated blog example using Next.js and Contentful.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
     </Head>
   )
 }
