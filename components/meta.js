c0edd3e7f6230227b0c7acdfd26aa6cc0552108d
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-01-17 21:45:07 +0100

    removed google

--- components/meta.js
+++ components/meta.js
@@ -32,19 +32,23 @@ export default function Meta() {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-{/* 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
+
 
       <meta
         name="description"
         content={`A statically generated blog example using Next.js and Contentful.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+
+
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" /> */}
+
     </Head>
   )
 }
