9f69e99d6921f446da3ed21fc83d8551bc8837df
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-03-09 17:51:53 +0100

    Updated copy

--- components/CarouselData.js
+++ components/CarouselData.js
@@ -1,21 +1,22 @@
 export const CarouselData = [
-    {
-      image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/c_fill,w_1600/v1636486533/ramy-keyframe_zdjnqu.jpg",
-        title: "LAW OF DESIRE",
-        blurb: "Fun that's as explosively bright and provocative as ever."
-    },
-    {
-      image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/c_fill,w_1600/v1636486532/spy_emrbfg.jpg",
-        title: "DESERT STORM STORY",
-        blurb: "A magical journey about the power of imagination."
-    },
-    {
-      image:
-        "https://res.cloudinary.com/annaga/image/upload/q_auto/f_avif/c_fill,w_1600/v1641050574/bear_gsipdp-q_auto_c_mfit_h_1600_w_2400_hyjg2c.jpg",
-        title: "MOVING UPSTREAM",
-        blurb: "Graceful comment on timely sociopolitical themes."
+  {
+    image:
+      "https://res.cloudinary.com/castles/image/upload/q_auto/f_avif/c_fill,w_1600/v1646843926/gatsby-cloudinary/gratitude_jwrhgj.png",
 
-    },
-  ];
+    title: "THE MINDFUL CHRISTIAN",
+    blurb: "Embracing life with acceptance and humility."
+  },
+  {
+    image:
+      "https://res.cloudinary.com/castles/image/upload/q_auto/f_avif/c_fill,w_1600/v1646843926/gatsby-cloudinary/meditate_lb3kjp.jpg",
+    title: "GRATITUDE",
+    blurb: "A spiritual journey about the power of imagination."
+  },
+  {
+    image:
+      "https://res.cloudinary.com/castles/image/upload/q_auto/f_avif/c_fill,w_1600/v1646843926/gatsby-cloudinary/psalm_ecdlo3.jpg",
+    title: "PREACH THE PSALMS",
+    blurb: "Graceful comment on timely sociopolitical themes."
+
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
 import Navbar from '../components/navbar'
-// import Carousel from '../components/Carousel'
+import Carousel from '../components/Carousel'
 import { getAllPostsForHome } from '../lib/api'
 //import { CMS_NAME } from '../lib/constants'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
-import IntroVideo from '../components/intro-video'
+//import IntroVideo from '../components/intro-video'
 
 export default function Index({ preview, allPosts }) {
-//  const heroPost = allPosts[0]
+  //  const heroPost = allPosts[0]
   //const morePosts = allPosts.slice(1)
 
   return (
@@ -20,13 +20,13 @@ export default function Index({ preview, allPosts }) {
         <Head>
           <title>Annaga Productions</title>
         </Head>
-        {/* <Carousel /> */}
-        <IntroVideo />
+        <Carousel />
+        {/* <IntroVideo /> */}
         <Navbar />
         <Container>
           {allPosts.length > 0 && <MoreStories posts={allPosts} />}
           <hr className="border-accent-2 mt-12" />
-          </Container>
+        </Container>
       </Layout>
     </>
   )
