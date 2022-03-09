9456938cf78b932076c725f2529569a05364c739
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-03-09 17:54:05 +0100

    Updated copy

--- components/CarouselData.js
+++ components/CarouselData.js
@@ -9,7 +9,7 @@ export const CarouselData = [
   {
     image:
       "https://res.cloudinary.com/castles/image/upload/q_auto/f_avif/c_fill,w_1600/v1646843926/gatsby-cloudinary/meditate_lb3kjp.jpg",
-    title: "GRATITUDE",
+    title: "PRACTICE GRATITUDE",
     blurb: "A spiritual journey about the power of imagination."
   },
   {
